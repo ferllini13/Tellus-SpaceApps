450abd991833e74d9f64f506fa5a29fb3f2f98ff
ferllini13 <ferllini_13@hotmail.com>
2018-10-21 02:38:55 -0600

    clase ataque y modificaciones genereales

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 from pygameGUI.cursor import cursor
 from pygameGUI.button import button
 from pygameGUI.loadImage import loadImage
-from logic.objects import objects
+from logic.objects import objects,attack
 
 pygame.mixer.init()
 #pygame.mixer.music.load("sound.mp3")
@@ -25,8 +25,6 @@ def main():
 
     #definiciones
     wallp=loadImage("backgrounds/f1.jpg",1)
-    x=0
-    y=0
     t1=loadImage("sprites/1.png")
     t2=loadImage("sprites/2.png")
     t3=loadImage("sprites/3.png")
@@ -36,28 +34,37 @@ def main():
 
     tt=[t1,t2,t3,t4,t5,t6]
     
-    p1=objects(tt,5)
+    p1=objects(tt,5,3)
+    p2=objects(tt,0,0)
+    p3=objects(tt,3,4)
+    p4=attack(tt,5,4)
 
-    x=0
-    y=0
-    p1.put(screen,x,y)
-    
+    p1.put(screen,0,0)
+    p2.put(screen,100,100)
+    p3.put(screen,200,200)
     
     while True:
         screen.blit(wallp,(0,0))
         myCursor.update()
-        p1.move(screen, myCursor)
         events=pygame.event.get()
         for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key==K_DOWN:
+                    p4.state = True
             if event.type==pygame.MOUSEBUTTONDOWN:
                 for obj in objects.instances:
                     if myCursor.colliderect(obj.rect):
                         obj.state= not obj.state
-            
+                        #obj.delete(obj.instances)
+                        #counter = 0
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        p1.update(screen)
+        for obj in attack.instances:
+            if(obj.state): obj.move(screen)
+        for obj in objects.instances:
+            obj.update(screen)
+            obj.move(screen, myCursor)
         pygame.display.flip()
         time.sleep(0.07)
 
--- logic/objects.py
+++ logic/objects.py
@@ -1,14 +1,99 @@
 import pygame
+import time
 from pygameGUI.cursor import cursor
 
-class objects(pygame.sprite.Sprite):#se crea la clase para los botones
-    instances=[]
-    def __init__(self,pics,cant):#se define cada boton con dos imagenes y las coordenadas
+
+class attack(pygame.sprite.Sprite):
+    tattack=[]
+    battack=[]
+    instances = []
+    pics = None 
+    cant = None
+    pos = None
+    state = None
+    type = None
+
+    def __init__(self,pics,cant,pType=0):#se define cada boton con dos imagenes y las coordenadas
         self.instances.append(self)
         self.pics=pics
         self.cant=cant
         self.pos=0
         self.state=False
+        self.type = pType
+        self.basic_pic=pics[self.pos] #se define una imagen bacica qie inicia como la no seleccionada
+        self.tattack=[(1036 , 180),
+            (1009 , 171) ,
+            (979 , 162),
+            (950 , 153),
+            (924 , 148),
+            (892 , 146),
+            (864 , 147),
+            (836 , 149),
+            (808 , 153),
+            (776 , 160),
+            (748 , 168),
+            (720 , 179) ,
+            (697 , 191),
+            (663 , 210),
+            (638 , 229),
+            (6167 , 246),
+            (598 , 267),
+            (582 , 292),
+            (572 , 316),
+            (562 , 343),
+            (561 , 367),
+            (560 , 392),
+            (564 , 418),
+            (577 , 448),
+            (592 , 475),
+            (625 , 450),
+            (623 , 521),
+            (646 , 540),
+            (669 , 553)]
+
+        self.battack=[(700 , 579),(725 , 590),(755 , 600),(784 , 610),(810 , 616),
+            (842 , 620),(869 , 621),(897 , 619),(925 , 616),(958 , 610),(986 , 604),
+            (1014 , 594),(1036 , 583),(1076 , 558),(1101 , 538),(1121 , 517),(1140 , 496),
+            (1155 , 471),(1165 , 446),(1175 , 420),(1177 , 395),(1179 , 369),(1173 , 341),
+            (1167 , 315),(1151 , 287),(1133 , 263),(1115 , 242),(1092 , 223),(1069 , 210)]
+            
+        self.poss=0
+
+    def put(self,screen, xy):
+        self.rect=self.basic_pic.get_rect()
+        self.rect.left,self.rect.top= xy
+        screen.blit(self.basic_pic,self.rect)
+
+    def move(self,screen):
+        if (self.poss>=len(self.battack)-1):
+            self.state = False
+            self.put(screen,(2000,2000))    
+        else:
+            self.poss=self.poss+1    
+        self.put(screen,self.battack[self.poss])
+        time.sleep(0.03)
+    
+    def delete(self,instances):
+        instances.remove(self)
+        del self
+        
+
+
+class objects(pygame.sprite.Sprite):#se crea la clase para los botones
+    instances=[]
+    pics = None 
+    cant = None
+    pos = None
+    state = None
+    type = None
+
+    def __init__(self,pics,cant,pType=0):#se define cada boton con dos imagenes y las coordenadas
+        self.instances.append(self)
+        self.pics=pics
+        self.cant=cant
+        self.pos=0
+        self.state=False
+        self.type = pType
         self.basic_pic=pics[self.pos] #se define una imagen bacica qie inicia como la no seleccionada
   
     def put(self,screen,x,y):
@@ -17,10 +102,11 @@ class objects(pygame.sprite.Sprite):#se crea la clase para los botones
         screen.blit(self.basic_pic,self.rect)
 
     def update(self,screen):
-        if (self.pos>=self.cant):
-            self.pos=0
-        else:
-            self.pos=self.pos+1
+        if(self.type == 3 or self.type == 4):
+            if (self.pos>=self.cant):
+                self.pos=0
+            else:
+                self.pos=self.pos+1
         self.basic_pic=self.pics[self.pos]
         screen.blit(self.basic_pic,self.rect)
 
@@ -28,4 +114,7 @@ class objects(pygame.sprite.Sprite):#se crea la clase para los botones
         if (self.state):
             x,y = pygame.mouse.get_pos()    
             self.put(screen,x,y)
-
+    
+    def delete(self,instances):
+        instances.remove(self)
+        del self
